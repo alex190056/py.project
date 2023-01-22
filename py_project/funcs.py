5cb0af3219d71f863c1f3346bcbb4739e06be63c
drofa <drofa528@bk.ru>
2023-01-22 18:25:50 +0500

    Change funcs.py: add function "subtract_two_numbers"

--- py_project/funcs.py
+++ py_project/funcs.py
@@ -2,3 +2,6 @@ def add_two_numbers(num_1: float, num_2: float) -> float:
     result = num_1 + num_2
     return result
 
+def subtract_two_numbers(num_1: float, num_2: float) -> float:
+    result = num_1 - num_2
+    return result
