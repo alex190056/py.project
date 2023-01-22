221d7c7e08c978a429f61fa22a2c763b15336c32
drofa <drofa528@bk.ru>
2023-01-22 18:37:52 +0500

    call function "subtract_two_numbers"

--- py_project/main.py
+++ py_project/main.py
@@ -1,4 +1,5 @@
-from funcs import add_two_numbers
+from funcs import add_two_numbers, subtract_two_numbers
 
 
 print(add_two_numbers(num_1=1.5, num_2=1))
+print(subtract_two_numbers(num_1=1, num_2=0.3))
